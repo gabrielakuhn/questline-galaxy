eabc62055aea2e582f16cc41062967efbd88559d
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-08 15:58:46 +0200

    installing nativewind

---
+++ apps/app/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "galaxy-dark-blue": "#00076f",
+        "galaxy-bright-blue": "#46A8E4",
+        "galaxy-baby-blue": "#7CD1EB",
+        "galaxy-blue": "#00076f",
+        "galaxy-violet": "#44008b",
+        "galaxy-purple": "#9f45b0",
+        "galaxy-pink": "#e54ed0",
+        "galaxy-rose": "#ffe4f2",
+      },
+    },
+  },
+  plugins: [],
+}
