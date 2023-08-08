9f67e597ab4dd7cfd5613e86694b32421df46802
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-08 16:38:33 +0200

    implemented first styles

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button } from "@/components/Button";
+import { StatusBar } from "expo-status-bar";
+import { Text, View, Alert } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="flex-1 items-center justify-center bg-white space-y-10">
+      <Text className="text-lg p-3">Hi!</Text>
+      <Text>Questline app met Typescript en Nativewind!</Text>
+      <View className="border border-slate-400 border-dashed rounded-2xl p-7 items-center space-y-6">
+        <Text>Color test</Text>
+        <View className="flex flex-row">
+          <View className="basis-1/6 h-20 bg-galaxy-dark-blue" />
+          <View className="basis-1/6 h-20 bg-galaxy-violet" />
+          <View className="basis-1/6 h-20 bg-galaxy-purple" />
+          <View className="basis-1/6 h-20 bg-galaxy-pink" />
+          <View className="basis-1/6 h-20 bg-galaxy-rose" />
+        </View>
+        <View className="flex flex-row">
+          <View className="basis-1/12 h-20 bg-galaxy-dark-blue" />
+          <View className="basis-1/12 h-20 bg-galaxy-bright-blue" />
+          <View className="basis-1/12 h-20 bg-galaxy-baby-blue" />
+          <View className="basis-1/12 h-20 bg-galaxy-violet" />
+          <View className="basis-1/12 h-20 bg-galaxy-purple" />
+          <View className="basis-1/12 h-20 bg-galaxy-pink" />
+          <View className="basis-1/12 h-20 bg-galaxy-rose" />
+        </View>
+      </View>
+      <View>
+        <Button title="Press me" onPress={() => Alert.alert("Ik ben een Alert!")}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
