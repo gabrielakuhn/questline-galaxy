924a7777b74b3c7993647a723d4bdfb7c456b657
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-11 11:52:09 +0200

    renamed menu props interface

--- apps/app/src/components/Menu/MenuButton.tsx
+++ apps/app/src/components/Menu/MenuButton.tsx
@@ -1,12 +1,13 @@
 import { Icon } from "@rneui/base";
 import { TouchableOpacity } from "react-native";
 
-interface MenuButton {
+interface MenuButtonProps {
   icon: string;
   iconType?: string;
   onPress: () => void;
 }
-export const MenuButton = ({ icon, iconType, onPress }: MenuButton) => {
+
+export const MenuButton = ({ icon, iconType, onPress }: MenuButtonProps) => {
   return (
     <TouchableOpacity onPress={onPress} className="p-8 pt-5">
       <Icon name={icon} type={iconType} />
