d145ef8a5236488c84c3229812b286f0a024b348
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-11 13:11:43 +0200

    added styles

--- apps/app/src/components/Menu/Menu.tsx
+++ apps/app/src/components/Menu/Menu.tsx
@@ -3,8 +3,8 @@ import { MenuButton } from "./MenuButton"
 
 export const Menu = () => {
   return (
-    <View className="bg-slate-100 absolute bottom-0 w-full pb-4 px-6">
-      <View className="flex flex-row justify-between">
+    <View className="bg-slate-100 absolute bottom-0 w-full border-t border-slate-200">
+      <View className="flex flex-row justify-center">
         <MenuButton icon="home" iconType="ionicon" onPress={() => Alert.alert("3...")}/>
         <MenuButton icon="rocket-sharp" iconType="ionicon" onPress={() => Alert.alert("2...")}/>
         <MenuButton icon="user-friends" iconType="font-awesome-5" onPress={() => Alert.alert("Lets Go!")}/>
