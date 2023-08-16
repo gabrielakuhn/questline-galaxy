21d807d6adbceb25b7c3b7a4d05a5660aea9f5b3
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-16 19:36:51 +0200

    reverted home content

--- apps/app/src/Screens/Home.tsx
+++ apps/app/src/Screens/Home.tsx
@@ -4,16 +4,25 @@ import { Menu } from "../components/Menu/Menu"
 export const Home = ({ navigation }: any) => {
   return (
     <View className="flex-1 items-center justify-center bg-white space-y-10">
-      <View className="flex-1 items-center justify-center bg-white space-y-10">
-        <View className="border border-slate-400 border-dashed rounded-2xl p-7 items-center space-y-6">
-          <Text>Color test</Text>
-          <View className="flex flex-row">
-            <View className="basis-1/6 h-20 bg-galaxy-dark-blue" />
-            <View className="basis-1/6 h-20 bg-galaxy-violet" />
-            <View className="basis-1/6 h-20 bg-galaxy-purple" />
-            <View className="basis-1/6 h-20 bg-galaxy-pink" />
-            <View className="basis-1/6 h-20 bg-galaxy-rose" />
-          </View>
+      <Text className="text-lg p-3">Hi!</Text>
+      <Text>Questline app met Typescript en Nativewind!</Text>
+      <View className="border border-slate-400 border-dashed rounded-2xl p-7 items-center space-y-6">
+        <Text>Color test</Text>
+        <View className="flex flex-row">
+          <View className="basis-1/6 h-20 bg-galaxy-dark-blue" />
+          <View className="basis-1/6 h-20 bg-galaxy-violet" />
+          <View className="basis-1/6 h-20 bg-galaxy-purple" />
+          <View className="basis-1/6 h-20 bg-galaxy-pink" />
+          <View className="basis-1/6 h-20 bg-galaxy-rose" />
+        </View>
+        <View className="flex flex-row">
+          <View className="basis-1/12 h-20 bg-galaxy-dark-blue" />
+          <View className="basis-1/12 h-20 bg-galaxy-bright-blue" />
+          <View className="basis-1/12 h-20 bg-galaxy-baby-blue" />
+          <View className="basis-1/12 h-20 bg-galaxy-violet" />
+          <View className="basis-1/12 h-20 bg-galaxy-purple" />
+          <View className="basis-1/12 h-20 bg-galaxy-pink" />
+          <View className="basis-1/12 h-20 bg-galaxy-rose" />
         </View>
       </View>
       <Menu navigation={navigation} />
