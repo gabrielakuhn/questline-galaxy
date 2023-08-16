93103370032d6c22f6d895359ee63c41f50d32bb
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-16 19:31:20 +0200

    added navigation to menu

--- apps/app/src/components/Menu/Menu.tsx
+++ apps/app/src/components/Menu/Menu.tsx
@@ -1,23 +1,25 @@
-import { Alert, View } from "react-native";
+import { View } from "react-native";
 import { MenuButton } from "./MenuButton";
 
-export const Menu = () => {
+export const Menu = ({ navigation }: any) => {
+  console.log("navigation menus", navigation);
+
   return (
     <View className="absolute bottom-0 bg-slate-100 border-t border-slate-200 flex flex-row justify-center">
       <MenuButton
         icon="home"
         iconType="ionicon"
-        onPress={() => Alert.alert("3...")}
-      />
-      <MenuButton
-        icon="rocket-sharp"
-        iconType="ionicon"
-        onPress={() => Alert.alert("2...")}
+        onPress={() => navigation.navigate("Home")}
       />
       <MenuButton
         icon="user-friends"
         iconType="font-awesome-5"
-        onPress={() => Alert.alert("Lets Go!")}
+        onPress={() => navigation.navigate("Friends", { name: "Goldy" })}
+      />
+      <MenuButton
+        icon="rocket-sharp"
+        iconType="ionicon"
+        onPress={() => navigation.navigate("Quests", { name: "Maya" })}
       />
     </View>
   )
