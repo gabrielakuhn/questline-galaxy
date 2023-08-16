9c23e2da7577e5dc5c688a537bdcf3c4dd1542b1
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-16 19:31:20 +0200

    added navigation to menu

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,10 +1,9 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native'
+import {createNativeStackNavigator} from '@react-navigation/native-stack'
 
-import { Menu } from "@/components/Menu/Menu";
-import { Text, View } from "react-native";
-import { Home } from '@/components/Home';
-import { Profile } from '@/components/Profile';
+import { Home } from '@/Screens/Home'
+import { Friends } from '@/Screens/Friends'
+import { Quests } from '@/Screens/Quests';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,24 +14,18 @@ export default function App() {
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{title: 'Welcome'}}
         />
-        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen
+          name="Friends"
+          component={Friends}
+          options={{title: 'Your Friends'}}
+        />
+        <Stack.Screen
+          name="Quests"
+          component={Quests}
+          options={{title: 'Your Quests'}}
+        />
       </Stack.Navigator>
-
-    <View className="flex-1 items-center justify-center bg-white space-y-10">
-      <View className="border border-slate-400 border-dashed rounded-2xl p-7 items-center space-y-6">
-        <Text>Color test</Text>
-        <View className="flex flex-row">
-          <View className="basis-1/6 h-20 bg-galaxy-dark-blue" />
-          <View className="basis-1/6 h-20 bg-galaxy-violet" />
-          <View className="basis-1/6 h-20 bg-galaxy-purple" />
-          <View className="basis-1/6 h-20 bg-galaxy-pink" />
-          <View className="basis-1/6 h-20 bg-galaxy-rose" />
-        </View>
-      </View>
-      <Menu/>
-    </View>
     </NavigationContainer>
   );
 }
