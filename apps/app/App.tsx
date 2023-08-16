2357e567f78d12c530e0ff7ed9190632fbb7ea8d
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-16 19:34:37 +0200

    hidden navigation header

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,31 +1,24 @@
-import {NavigationContainer} from '@react-navigation/native'
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import { Home } from '@/Screens/Home'
-import { Friends } from '@/Screens/Friends'
-import { Quests } from '@/Screens/Quests';
+import { Home } from "@/Screens/Home";
+import { Friends } from "@/Screens/Friends"
+import { Quests } from "@/Screens/Quests"
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-        />
-        <Stack.Screen
-          name="Friends"
-          component={Friends}
-          options={{title: 'Your Friends'}}
-        />
-        <Stack.Screen
-          name="Quests"
-          component={Quests}
-          options={{title: 'Your Quests'}}
-        />
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Friends" component={Friends} />
+        <Stack.Screen name="Quests" component={Quests} />
       </Stack.Navigator>
     </NavigationContainer>
-  );
+  )
 }
