9c8732165821b1c4b4fc264a5be64e5931d1a2a8
gabrielakuhn <gbr.kuhn@gmail.com>
2023-08-21 13:26:34 +0200

    renamed Friends screen to Crew

--- apps/app/src/Screens/Crew.tsx
+++ apps/app/src/Screens/Crew.tsx
@@ -4,12 +4,12 @@ import { View, Text } from "react-native"
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootScreensParamList, Screen } from "./Types/Screens";
 
-type Props = NativeStackScreenProps<RootScreensParamList, Screen.Friends>
+type Props = NativeStackScreenProps<RootScreensParamList, Screen.Crew>
 
-export const Friends = ({navigation, route}: Props) => {
+export const Crew = ({navigation, route}: Props) => {
   return (
     <View className="flex-1 items-center justify-center bg-orange-200 space-y-10">
-      <Text>This is {route.params.name}'s friends</Text>
+      <Text>This is {route.params.name}'s Crew</Text>
       <Menu navigation={navigation} route={route} />
     </View>
   )
