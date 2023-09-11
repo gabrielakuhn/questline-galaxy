23229ca79ea96dfa00487d857353ef47af3b97a1
gabrielakuhn <gbr.kuhn@gmail.com>
2023-09-11 16:23:35 +0200

    adding trip to redux state

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -1,25 +1,29 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Provider as ReduxProvider } from "react-redux";
 
 import { Home } from "@/screens/Home";
 import { Crew } from "@/screens/Crew";
 import { Quests } from "@/screens/Quests";
 import { RootScreensParamList, Screen } from "@/screens/Models/Screens";
+import { store } from "@/store/store";
 
 const Stack = createNativeStackNavigator<RootScreensParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name={Screen.Home} component={Home} />
-        <Stack.Screen name={Screen.Crew} component={Crew} />
-        <Stack.Screen name={Screen.Quests} component={Quests} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ReduxProvider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name={Screen.Home} component={Home} />
+          <Stack.Screen name={Screen.Crew} component={Crew} />
+          <Stack.Screen name={Screen.Quests} component={Quests} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ReduxProvider>
   );
 }
