6dc5456506f19f76f03823e419e5566f7a378da1
gabrielakuhn <gbr.kuhn@gmail.com>
2023-09-15 14:57:57 +0200

    making left pad more generic

--- apps/app/src/infrastructure/utilities/index.ts
+++ apps/app/src/infrastructure/utilities/index.ts
@@ -1,1 +1,15 @@
-export const leftPad = (n: number): string => n.toString().padStart(2, "0");
+/**
+ * Based on String.prototype.padStart()
+ * @param value The value to be pad.
+ * @param targetLength The length of the resulting string once the current str has been padded.
+ * @param padString The string to pad the current str with.
+ * @returns
+ */
+
+export const leftPad = (
+  value: number | string,
+  targetLength: number = 2,
+  padString: string = "0"
+): string => {
+  return value.toString().padStart(targetLength, padString);
+};
