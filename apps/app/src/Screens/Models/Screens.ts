02c2899986412741814555d7bfb281a77060af2c
gabrielakuhn <gbr.kuhn@gmail.com>
2023-09-15 15:32:57 +0200

    created add trip screen

--- apps/app/src/Screens/Models/Screens.ts
+++ apps/app/src/Screens/Models/Screens.ts
@@ -1,12 +1,13 @@
 export enum Screen {
-    Home = "Home",
-    Crew = "Crew",
-    Quests = "Quests"
-} 
-
-export type RootScreensParamList = {
-    [Screen.Home]: undefined;
-    [Screen.Crew]: { name: string };
-    [Screen.Quests]: { name: string };
+  Home = "Home",
+  Crew = "Crew",
+  Quests = "Quests",
+  CreateTrip = "CreateTrip",
 }
 
+export type RootScreensParamList = {
+  [Screen.Home]: undefined;
+  [Screen.Crew]: { name: string };
+  [Screen.Quests]: { name: string };
+  [Screen.CreateTrip]: undefined;
+};
