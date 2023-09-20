fad6fdc31beab044cb91f8d4dd9589a4c00c8790
gabrielakuhn <gbr.kuhn@gmail.com>
2023-09-20 10:03:56 +0200

    adde navigation to Home and scrollview

--- apps/app/src/Screens/Home.tsx
+++ apps/app/src/Screens/Home.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import { Menu } from "../domain/Menu/Menu";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootScreensParamList, Screen } from "./Models/Screens";
@@ -8,15 +8,11 @@ import { Trip } from "@/domain/Trip/Trip";
 import { useAppDispatch, useAppSelector } from "@/store/Infrastructure/Hooks";
 import {
   fetchTrips,
-  addTrip as addTripToState,
   removeTrip as removeTripFromState,
 } from "@/store/Domain/trips-slice";
 import { RootState } from "@/store/store";
 import { StoreStatus } from "@/store/Infrastructure/Status";
-import {
-  removeTripFromLocalStorage,
-  storeTripIntoLocalStorage,
-} from "@/domain/Trip/Application/TripsStorage";
+import { removeTripFromLocalStorage } from "@/domain/Trip/Application/TripsStorage";
 
 type Props = NativeStackScreenProps<RootScreensParamList>;
 
@@ -39,26 +35,31 @@ export const Home = ({ navigation, route }: Props) => {
     }
   });
 
+  /**
+   * TODO: Create a generic structure for screens with <View><ScrollView><ContentView/></ScrollView><Menu/></View>
+   */
+
   return (
-    <View className="flex-1 items-center justify-center bg-white space-y-10">
-      <Text className="text-lg p-3">Hi!</Text>
-      <Text>Questline app met Typescript en Nativewind!</Text>
-      <View className="space-y-6">
-        {trips.map((trip) => (
-          <View
-            key={trip.id}
-            className="border border-slate-400 border-dashed rounded-2xl"
-          >
-            <Trip trip={trip} remove={removeTrip} />
+    <View className="flex-1 bg-white">
+      <ScrollView>
+        <View className="pt-24 px-6 pb-28 space-y-6">
+          <Text>Questline app met Typescript en Nativewind!</Text>
+          {trips.map((trip) => (
+            <View
+              key={trip.id}
+              className="border border-slate-400 border-dashed rounded-2xl"
+            >
+              <Trip trip={trip} remove={removeTrip} />
+            </View>
+          ))}
+          <View>
+            <Button
+              onPress={() => navigation.navigate(Screen.CreateTrip)}
+              title="Add Trip"
+            />
           </View>
-        ))}
-      </View>
-      <View>
-        <Button
-          onPress={() => navigation.navigate(Screen.CreateTrip)}
-          title="Add Trip"
-        />
-      </View>
+        </View>
+      </ScrollView>
       <Menu navigation={navigation} route={route} />
     </View>
   );
