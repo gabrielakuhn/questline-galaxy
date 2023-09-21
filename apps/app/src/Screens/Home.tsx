702e363eaac61f514cd00e0d0fdbb464508b4549
gabrielakuhn <gbr.kuhn@gmail.com>
2023-09-21 17:17:02 +0200

    getting base layout in screens

--- apps/app/src/Screens/Home.tsx
+++ apps/app/src/Screens/Home.tsx
@@ -1,5 +1,4 @@
-import { Text, View, ScrollView } from "react-native";
-import { Menu } from "../domain/Menu/Menu";
+import { Text, View } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootScreensParamList, Screen } from "./Models/Screens";
 import { Button } from "@/components/Button";
@@ -8,6 +7,7 @@ import { Trip } from "@/domain/Trip/Trip";
 import { useAppDispatch, useAppSelector } from "@/store/Infrastructure/Hooks";
 import { fetchTrips, getTripStore } from "@/store/Domain/trips-slice";
 import { StoreStatus } from "@/store/Infrastructure/Status";
+import { Layout } from "./Layout";
 
 type Props = NativeStackScreenProps<RootScreensParamList>;
 
@@ -21,32 +21,25 @@ export const Home = ({ navigation, route }: Props) => {
     }
   });
 
-  /**
-   * TODO: Create a generic structure for screens with <View><ScrollView><ContentView/></ScrollView><Menu/></View>
-   */
-
   return (
-    <View className="flex-1 bg-white">
-      <ScrollView>
-        <View className="pt-24 px-6 pb-28 space-y-6">
-          <Text>Questline app met Typescript en Nativewind!</Text>
-          {trips.map((trip) => (
-            <View
-              key={trip.id}
-              className="border border-slate-400 border-dashed rounded-2xl"
-            >
-              <Trip trip={trip} />
-            </View>
-          ))}
-          <View>
-            <Button
-              onPress={() => navigation.navigate(Screen.CreateTrip)}
-              title="Add Trip"
-            />
+    <Layout navigation={navigation} route={route}>
+      <View className="space-y-8">
+        <Text>Questline app met Typescript en Nativewind!</Text>
+        {trips.map((trip) => (
+          <View
+            key={trip.id}
+            className="border border-slate-400 border-dashed rounded-2xl"
+          >
+            <Trip trip={trip} />
           </View>
+        ))}
+        <View>
+          <Button
+            onPress={() => navigation.navigate(Screen.CreateTrip)}
+            title="Add Trip"
+          />
         </View>
-      </ScrollView>
-      <Menu navigation={navigation} route={route} />
-    </View>
+      </View>
+    </Layout>
   );
 };
