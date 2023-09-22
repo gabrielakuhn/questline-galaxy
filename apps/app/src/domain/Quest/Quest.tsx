c82c0e38c99f842b99f73cc58d1a9b1dc25f4259
gabrielakuhn <gbr.kuhn@gmail.com>
2023-09-22 15:28:04 +0200

    implemented removeQuestFromState

--- apps/app/src/domain/Quest/Quest.tsx
+++ apps/app/src/domain/Quest/Quest.tsx
@@ -1,14 +1,40 @@
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import { Quest as QuestModel } from "./Models/Quest";
+import { useAppDispatch, useAppSelector } from "@/store/Infrastructure/Hooks";
+import {
+  getQuestStore,
+  remove as removeQuestFromState,
+} from "@/store/Domain/quest-slice";
+import { StorageKey } from "@/infrastructure/storage/localstorage/Keys";
+import { removeValueFromStoredArray } from "@/infrastructure/storage/Application/Helper";
+import { Timer } from "@/components/Timer";
 
 interface Props {
   quest: QuestModel;
 }
 
 export const Quest = ({ quest }: Props) => {
+  const dispatch = useAppDispatch();
+  const { quests } = useAppSelector(getQuestStore);
+
+  const deleteTrip = async (id: string): Promise<void> => {
+    const isRemoved = await removeValueFromStoredArray(
+      quests,
+      id,
+      StorageKey.Quests
+    );
+
+    if (isRemoved) {
+      dispatch(removeQuestFromState(id));
+    }
+  };
   return (
     <View className="flex flex-row p-6 items-center">
       <Text className="font-bold pr-3">{quest.name}</Text>
+      <Timer start={new Date(quest.start)} />
+      <View className="mb-1">
+        <Button onPress={() => deleteTrip(quest.id)} title="x"></Button>
+      </View>
     </View>
   );
 };
