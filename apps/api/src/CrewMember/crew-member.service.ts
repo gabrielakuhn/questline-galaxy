f562e20a73cdafb733c6c0867e37631fedf9867f
gabrielakuhn <gbr.kuhn@gmail.com>
2023-10-25 17:06:32 +0200

    created prisma, crew and crew member service

---
+++ apps/api/src/CrewMember/crew-member.service.ts
@@ -0,0 +1,34 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CrewMember, Prisma } from '@prisma/client';
+import { PrismaService } from 'src/Prisma/prisma.service';
+
+@Injectable()
+export class CrewMemberService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async findAll(): Promise<CrewMember[]> {
+    return await this.prismaService.crewMember.findMany();
+  }
+
+  async findById(crewMemberId: CrewMember['id']): Promise<CrewMember> {
+    const crewMember = await this.prismaService.crewMember.findUnique({
+      where: {
+        id: crewMemberId,
+      },
+    });
+
+    if (crewMember === null) {
+      throw new NotFoundException();
+    }
+
+    return crewMember;
+  }
+
+  async createCrewMember(
+    data: Prisma.CrewMemberCreateInput,
+  ): Promise<CrewMember> {
+    return this.prismaService.crewMember.create({
+      data,
+    });
+  }
+}
--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,12 +1,56 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+
 import { AppService } from './app.service';
+import { CrewService } from './Crew/crew.service';
+import { CrewMemberService } from './CrewMember/crew-member.service';
+import {
+  Crew as CrewModel,
+  CrewMember as CrewMemberModel,
+  Prisma,
+} from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly crewService: CrewService,
+    private readonly crewMemberService: CrewMemberService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('crew')
+  async getAllCrew(): Promise<CrewModel[]> {
+    return await this.crewService.findAll();
+  }
+
+  @Get('crewMember')
+  async getAllCrewMember(): Promise<CrewMemberModel[]> {
+    return await this.crewMemberService.findAll();
+  }
+
+  @Get('crew/:id')
+  async getCrewById(@Param('id') id: number): Promise<CrewModel> {
+    return this.crewService.findById(id);
+  }
+
+  @Get('crewMember/:id')
+  async getCrewMemberById(@Param('id') id: number): Promise<CrewMemberModel> {
+    return this.crewMemberService.findById(id);
+  }
+  @Post('crew')
+  async createCrew(
+    @Body() crewData: Prisma.CrewCreateInput,
+  ): Promise<CrewModel> {
+    return this.crewService.createCrew(crewData);
+  }
+  @Post('crewMember')
+  async createCrewMemr(
+    @Body() crewMemberData: Prisma.CrewMemberCreateInput,
+  ): Promise<CrewModel> {
+    return this.crewMemberService.createCrewMember(crewMemberData);
   }
 }
