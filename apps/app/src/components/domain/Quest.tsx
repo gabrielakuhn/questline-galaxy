16de7be0153013f62c399476527a3c464e6536d0
gabrielakuhn <gbr.kuhn@gmail.com>
2023-11-14 22:48:17 +0100

    implemented finish quest

--- apps/app/src/components/domain/Quest.tsx
+++ apps/app/src/components/domain/Quest.tsx
@@ -1,26 +1,32 @@
 import { Button, Text, View } from "react-native";
+import { CheckBox } from "@rneui/themed";
+
 import { Timer } from "@/components/generic/Timer";
-import { Quest as QuestModel } from "@/types";
+import { Quest as QuestType } from "@/types";
 
 import {
   StorageKey,
+  modifyValueIntoStoredArray,
   removeValueFromStoredArray,
 } from "@/infrastructure/storage";
 import { useAppDispatch, useAppSelector } from "@/store/infrastructure";
 import {
   getQuestStore,
+  modifyQuest as modifyQuestFromState,
   removeQuest as removeQuestFromState,
 } from "@/store/domain";
+import { QuestInstance } from "@/constructors/domain/Quest";
 
 interface Props {
-  quest: QuestModel;
+  quest: QuestType;
 }
 
 export const Quest = ({ quest }: Props) => {
+  const thisQuest = new QuestInstance(quest);
   const dispatch = useAppDispatch();
   const { quests } = useAppSelector(getQuestStore);
 
-  const deleteTrip = async (id: string): Promise<void> => {
+  const deleteQuest = async (id: string): Promise<void> => {
     const isRemoved = await removeValueFromStoredArray(
       quests,
       id,
@@ -31,12 +37,40 @@ export const Quest = ({ quest }: Props) => {
       dispatch(removeQuestFromState(id));
     }
   };
+
+  const finishQuest = async (id: string): Promise<void> => {
+    const indexToModify = quests.findIndex((quest) => quest.id == id);
+    const modifiedQuest = {
+      ...quests[indexToModify],
+      end: new Date().toString(),
+    };
+
+    const isStored = await modifyValueIntoStoredArray(
+      quests,
+      modifiedQuest,
+      StorageKey.Quests
+    );
+
+    if (isStored) {
+      dispatch(modifyQuestFromState(modifiedQuest));
+    }
+  };
+
   return (
     <View className="flex flex-row p-6 items-center">
       <Text className="font-bold pr-3">{quest.name}</Text>
-      <Timer start={new Date(quest.start)} />
+      <Timer
+        start={quest.start}
+        end={thisQuest.isFinished() ? quest.end : undefined}
+      />
+      <CheckBox
+        checked={thisQuest.isFinished()}
+        disabled={thisQuest.isFinished()}
+        onPress={() => finishQuest(quest.id)}
+        size={24}
+      />
       <View className="mb-1">
-        <Button onPress={() => deleteTrip(quest.id)} title="x"></Button>
+        <Button onPress={() => deleteQuest(quest.id)} title="x"></Button>
       </View>
     </View>
   );
