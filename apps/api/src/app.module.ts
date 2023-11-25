864c210490ef4bf4d1fe4d3cb2d288595b355126
gabrielakuhn <gbr.kuhn@gmail.com>
2023-11-25 21:27:53 +0100

    creates crew, crew member and prisma service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CrewService } from './Crew/crew.service';
+import { CrewMemberService } from './CrewMember/crew-member.service';
+import { PrismaService } from './Prisma/prisma.service';
 
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CrewService, CrewMemberService, PrismaService],
 })
 export class AppModule {}
